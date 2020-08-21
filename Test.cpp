c46530196d4ad43fdc49b3a717c9caadf282538c
jerrypvnrt <68635359+jerrypvnrt@users.noreply.github.com>
2020-08-21 18:14:34 +0800

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> a[6];
+bool dfs_used[6] = {0};
+void dfs(int id) {
+	for (int i = 0; i < a[id].size(); ++i) {
+		if (!dfs_used[id]) {
+			dfs_used[id] = 1;
+			cout << id << " ";
+			dfs(a[id][i]);
+		}
+	}
+}
+
+int main() {
+	for (int i = 0; i < 7; ++i) {
+		int x, y; cin >> x >> y;
+		a[x].push_back(y);
+		a[y].push_back(x);
+	}
+	for (int i = 1; i < 6; ++i) {
+		cout << i << ": ";
+		for (int j = 0; j < a[i].size(); ++j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	dfs(1); cout << endl;
+	bool used[6] = { 0 };
+	queue<int> bfs;
+	bfs.push(1);
+	used[1] = 1;
+	while (!bfs.empty()) {
+		int x=bfs.front();
+		cout << x << " ";
+		for (int i = 0; i < a[x].size(); ++i) {
+			if (used[a[x][i]]) continue;
+			bfs.push(a[x][i]);
+			used[a[x][i]] = 1;
+		}
+		bfs.pop();
+	}
+	cout << endl;
+
+}
